4f8d4d6e0e87ebfa5ad1d0c0cf336af2a726448b
Chua Zi Heng <chuaziheng98@gmail.com>
2022-06-11 23:54:25 +0800

    WIP: Broadcast component

---
+++ components/pages/CreateBroadcast/CreateBroadcast.js
@@ -0,0 +1,27 @@
+import * as React from 'react';
+import { TextInput, StyleSheet } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+export default CreateBroadcastScreen = ()  => {
+    const [text, onChangeText] = React.useState(null);
+
+    return (
+        <SafeAreaView>
+            <TextInput
+                style={styles.input}
+                onChangeText={onChangeText}
+                value={text}
+                placeholder="Title"
+            />
+        </SafeAreaView>
+    );
+}
+
+const styles = StyleSheet.create({
+    input: {
+      height: 40,
+      margin: 12,
+      borderWidth: 1,
+      padding: 10,
+    },
+  });
