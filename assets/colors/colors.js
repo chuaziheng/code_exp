56368bac21b2b2192d8023e1ec5bd05768edd97b
Chua Zi Heng <chuaziheng98@gmail.com>
2022-06-12 12:13:16 +0800

    Added: Broadcast FE

---
+++ assets/colors/colors.js
@@ -0,0 +1,14 @@
+const colors = {
+	background: '#FBFCFE',
+	textDark: '#0C0D0E',
+	primary: '#F5CA48',
+	secondary: '#F26C68',
+	textLight: '#CDCDCD',
+	textAccent: '#F35D38',
+	white: '#FFF',
+	black: '#000',
+	grey: '#F5F5F5',
+	textGrey: '#737373',
+      };
+
+export default colors;
--- components/pages/CreateBroadcast/CreateBroadcast.js
+++ components/pages/CreateBroadcast/CreateBroadcast.js
@@ -1,35 +1,85 @@
 import * as React from 'react';
-import { TextInput, StyleSheet } from 'react-native';
+import { TextInput, StyleSheet, View, Text, Button, Alert  } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import colors from '../../../assets/colors/colors';
 
 const CreateBroadcastScreen = ()  => {
-    const [text, onChangeText] = React.useState(null);
+    const [titleText, onChangeTitle] = React.useState("");
+    const [messageText, onChangeMessage] = React.useState("");
 
     return (
         <SafeAreaView>
+            <View style={styles.headerWrapper}>
+            {/* Header */}
+            <View style={styles.titlesWrapper}>
+                {/* <Text style={styles.titlesSubtitle}>Hello,</Text> */}
+                <Text style={styles.titlesTitle}>Broadcast</Text>
+            </View>
+
+            </View>
             <TextInput
-                style={styles.input}
-                onChangeText={onChangeText}
-                value={text}
+                style={styles.inputTitle}
+                onChangeText={onChangeTitle}
+                value={titleText}
                 placeholder="Title"
             />
             <TextInput
                 style={styles.input}
-                onChangeText={onChangeText}
-                value={text}
-                placeholder="Title"
+                onChangeText={onChangeMessage}
+                value={messageText}
+                placeholder="ENTER MESSAGE"
+            />
+            <Button
+                style={styles.submitButton}
+                title="Submit"
+                onPress={() => alert('Simple Button pressed')}
+                // onPress={() => Alert.alert('Simple Button pressed')}
             />
         </SafeAreaView>
     );
 }
 
 const styles = StyleSheet.create({
-    input: {
+    headerWrapper: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        paddingHorizontal: 15,
+        paddingTop: 20,
+        alignItems: 'right',
+      },
+    inputTitle: {
       height: 40,
       margin: 12,
       borderWidth: 1,
       padding: 10,
     },
+    input: {
+        height: 140,
+        margin: 12,
+        borderWidth: 1,
+        padding: 10,
+      },
+    titlesSubtitle: {
+        // fontFamily: 'Montserrat-Regular',
+        fontSize: 25,
+        fontWeight: '600',
+        color: colors.textDark,
+      },
+      titlesTitle: {
+        // fontFamily: 'Montserrat-Bold',
+        fontSize: 32,
+        color: colors.textDark,
+        fontWeight: '600',
+        justifyContent: "center",
+        alignItems: "center"
+      },
+      submitButton: {
+          flex: 1,
+          width: 30,
+          flexDirection: 'row',
+          alignItems: 'center',
+          justifyContent: 'center',
+      }
   });
 
 export default CreateBroadcastScreen;
